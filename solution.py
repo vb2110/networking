bec25a71ca423d06bda3d9bfada1e2d12ccf4a74
vb2110 <72482806+vb2110@users.noreply.github.com>
2020-10-07 02:54:32 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -14,7 +14,7 @@ def webServer(port=13331):
             f = open(filename[1:])
             outputdata = f.read() # reads the file contents Fill in start
             # connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
-            connectionSocket.send(bytes("HTTP/1.1 404 Not Found\r\n\r\n","UTF-8"))
+            connectionSocket.send(bytes("HTTP/1.1 200 OK\r\n\r\n","UTF-8"))
             for i in range(0, len(outputdata)):
                 connectionSocket.send(outputdata[i].encode())
             connectionSocket.send("\r\n".encode())
