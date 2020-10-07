59d0a2f8e0b070cd05f5ace67a8e388a8f9550c5
vb2110 <72482806+vb2110@users.noreply.github.com>
2020-10-07 02:37:36 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,38 +3,24 @@ from socket import *
 import sys # In order to terminate the program
 def webServer(port=13331):
     serverSocket = socket(AF_INET, SOCK_STREAM)
-    #Prepare a sever socket
-    #serverSocket.bind(('', port)) #
     serverSocket.bind(('', port))
-    #Fill in start
     serverSocket.listen(1)
-    #Fill in end
     while True:
-        #Establish the connection
         print('Ready to serve...')
         connectionSocket, addr = serverSocket.accept()  #Fill in start      #Fill in end
         try:
             message = connectionSocket.recv(4096)
             filename = message.split()[1]
-            ###
             f = open(filename[1:])
             outputdata = f.read() # reads the file contents Fill in start     #Fill in end
-            #Send one HTTP header line into socket
-            connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-            #Send the content of the requested file to the client
+            connectionSocket.send("\nHTTP/1.1 200 OK\r\n\r\n".encode('utf-8'))
             for i in range(0, len(outputdata)):
                 connectionSocket.send(outputdata[i].encode())
             connectionSocket.send("\r\n".encode())
             connectionSocket.close()
         except IOError:
-            #Send response message for file not found (404)
-            #Fill in start
-            connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
-            #Fill in end
-            #Close client socket
-            #Fill in start
+            connectionSocket.send("\nHTTP/1.1 404 Not Found\r\n\r\n".encode('utf-8'))
             connectionSocket.close()
-            #Fill in end
     serverSocket.close()
     sys.exit()  # Terminate the program after sending the corresponding data
 if __name__ == "__main__":
