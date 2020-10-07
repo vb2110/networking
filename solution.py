327d618a98ea6df262fda8bc9a16d71012203146
vb2110 <72482806+vb2110@users.noreply.github.com>
2020-10-07 02:24:46 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -20,7 +20,7 @@ def webServer(port=13331):
             f = open(filename[1:])
             outputdata = f.read() # reads the file contents Fill in start     #Fill in end
             #Send one HTTP header line into socket
-            connectionSocket.send("\nHTTP/1.1 200 OK\r\n\r\n".encode()) # for 200 OK
+            connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
             #Send the content of the requested file to the client
             for i in range(0, len(outputdata)):
                 connectionSocket.send(outputdata[i].encode())
@@ -29,7 +29,7 @@ def webServer(port=13331):
         except IOError:
             #Send response message for file not found (404)
             #Fill in start
-            connectionSocket.send("\nHTTP/1.1 404 not found\r\n\r\n".encode())
+            connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
             #Fill in end
             #Close client socket
             #Fill in start
