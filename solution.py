63624c4942062ece0fc5a14baff70d32aa1de3df
vb2110 <72482806+vb2110@users.noreply.github.com>
2020-10-10 19:35:25 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -57,9 +57,9 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send QUIT command and get server response.
     # Fill in start
-    semdQuit = "QUIT\r\n"
+    semdQuit = "QUIT \r\n"
     clientSocket.send(semdQuit.encode())
-    recv6 = clientSocket.recv(1024).decode()
+    # recv6 = clientSocket.recv(1024).decode()
     clientSocket.close()
     # Fill in end
 
