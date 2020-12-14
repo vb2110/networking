d68bf13f5f6f1d7799dc764a26da5fb494df392f
vb2110 <72482806+vb2110@users.noreply.github.com>
2020-12-13 21:12:12 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,156 +1,41 @@
-from socket import *
-import os
-import sys
-import struct
-import time
-import select
-import binascii
+### welcome_assignment_answers
+### Input - All eight questions given in the assignment.
+### Output - The right answer for the specific question.
 
-ICMP_ECHO_REQUEST = 8
-MAX_HOPS = 30
-TIMEOUT = 2.0
-TRIES = 1
-# The packet that we shall send to each router along the path is the ICMP echo
-# request packet, which is exactly what we had used in the ICMP ping exercise.
-# We shall use the same packet that we built in the Ping exercise
+def welcome_assignment_answers(question):
+   # The student doesn't have to follow the skeleton for this assignment.
+   # Another way to implement it is using "case" statements similar to C.
+   if question == "Are encoding and encryption the same? - Yes/No":
+       answer = "no"
+       return (answer)
+   elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+       answer = "no"
+       return (answer)
+   elif question == "Is it possible to decode a message without a key? - Yes/No":
+       answer = "no"
+       return (answer)
+   elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "no"
+        return (answer)
+   elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
+        answer = "42b76fe51778764973077a5a94056724"
+        return (answer)
+   elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "no"
+        return (answer)
+   elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
+        answer = 5
+        return (answer)
+   elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
+        answer = 4
+        return (answer)
+    
 
-def checksum(string):
-# In this function we make the checksum of our packet
-    csum = 0
-    countTo = (len(string) // 2) * 2
-    count = 0
 
-    while count < countTo:
-        thisVal = (string[count + 1]) * 256 + (string[count])
-        csum += thisVal
-        csum &= 0xffffffff
-        count += 2
+# Complete all the questions.
 
-    if countTo < len(string):
-        csum += (string[len(string) - 1])
-        csum &= 0xffffffff
 
-    csum = (csum >> 16) + (csum & 0xffff)
-    csum = csum + (csum >> 16)
-    answer = ~csum
-    answer = answer & 0xffff
-    answer = answer >> 8 | (answer << 8 & 0xff00)
-    return answer
-
-def build_packet():
-    #Fill in start
-    # In the sendOnePing() method of the ICMP Ping exercise ,firstly the header of our
-    # packet to be sent was made, secondly the checksum was appended to the header and
-    # then finally the complete packet was sent to the destination.
-
-    # Make the header in a similar way to the ping exercise.
-    myChecksum = 0
-    myPID = os.getpid() & 0xFFFF
-    # Append checksum to the header.
-    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, myPID, 1)
-    data = struct.pack("d", time.time())
-    myChecksum = checksum(header + data)
-    if sys.platform == 'darwin':
-        myChecksum = htons(myChecksum) & 0xffff
-    else:
-        myChecksum = htons(myChecksum)
-    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, myPID, 1)
-    # Don’t send the packet yet , just return the final packet in this function.
-    #Fill in end
-
-    # So the function ending should look like this
-
-    packet = header + data
-    return packet
-
-def get_route(hostname):
-    timeLeft = TIMEOUT
-    tracelist1 = [] #This is your list to use when iterating through each trace
-    tracelist2 = [] #This is your list to contain all traces
-
-    for ttl in range(1,MAX_HOPS):
-        for tries in range(TRIES):
-            destAddr = gethostbyname(hostname)
-            tracelist1 = []
-
-            #Fill in start
-            # Make a raw socket named mySocket
-            icmp = socket.getprotobyname("icmp")
-            mySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, icmp)
-            #Fill in end
-
-            mySocket.setsockopt(IPPROTO_IP, IP_TTL, struct.pack('I', ttl))
-            mySocket.settimeout(TIMEOUT)
-            try:
-                d = build_packet()
-                mySocket.sendto(d, (hostname, 0))
-                t= time.time()
-                startedSelect = time.time()
-                whatReady = select.select([mySocket], [], [], timeLeft)
-                howLongInSelect = (time.time() - startedSelect)
-                if whatReady[0] == []: # Timeout
-                    tracelist1.append("* * * Request timed out.")
-                    #Fill in start
-                    #You should add the list above to your all traces list
-                    tracelist2.append(tracelist1)
-                    #Fill in end
-                recvPacket, addr = mySocket.recvfrom(1024)
-                timeReceived = time.time()
-                timeLeft = timeLeft - howLongInSelect
-                if timeLeft <= 0:
-                    tracelist1.append("* * * Request timed out.")
-                    #Fill in start
-                    #You should add the list above to your all traces list
-                    tracelist2.append(tracelist2)
-                    #Fill in end
-            except timeout:
-                continue
-
-            else:
-                #Fill in start
-                #Fetch the icmp type from the IP packet
-                types = recvPacket[20]
-                #Fill in end
-                try: #try to fetch the hostname
-                    #Fill in start
-                    getHost = gethostbyaddr(addr[0])[0]
-                    #Fill in end
-                except herror:   #if the host does not provide a hostname
-                    #Fill in start
-                    getHost = ("host does not provide a hostname")
-                    #Fill in end
-
-                if types == 11:
-                    bytes = struct.calcsize("d")
-                    timeSent = struct.unpack("d", recvPacket[28:28 +
-                    bytes])[0]
-                    #Fill in start
-                    #You should add your responses to your lists here
-                    tracelist1.append("%d, %.0fms, %s, %s" %(ttl, (timeReceived -t)*1000, addr[0], getHost))
-                    tracelist2.append(tracelist1)
-                    #Fill in end
-                elif types == 3:
-                    bytes = struct.calcsize("d")
-                    timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
-                    #Fill in start
-                    #You should add your responses to your lists here
-                    tracelist1.append("%d, %.0fms, %s, %s" %(ttl, (timeReceived-t)*1000, addr[0], getHost))
-                    tracelist2.append(tracelist1)
-                    #Fill in end
-                elif types == 0:
-                    bytes = struct.calcsize("d")
-                    timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
-                    #Fill in start
-                    #You should add your responses to your lists here and return your list if your destination IP is met
-                    tracelist1.append(" %d   rtt=%.0f ms %s", ttl,(timeReceived -t)*1000, addr[0])
-                    tracelist2.append(tracelist1)
-                    return tracelist2
-                    #Fill in end
-                else:
-                    #Fill in start
-                    #If there is an exception/error to your if statements, you should append that to your list here
-                    tracelist2.append("Error")
-                    #Fill in end
-                break
-            finally:
-                mySocket.close()
+if __name__ == "__main__":
+   # use this space to debug and verify that the program works
+   debug_question = "Are encoding and encryption the same? - Yes/No"
+   print(welcome_assignment_answers(debug_question))
